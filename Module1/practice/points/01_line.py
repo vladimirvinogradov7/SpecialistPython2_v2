430cae9af25f76ae6a66f7c4c4837a361e3cb5ec
vladimirvinogradov7 <77646587+vladimirvinogradov7@users.noreply.github.com>
2021-03-20 13:25:13 +0300

    Update 01_line.py

--- Module1/practice/points/01_line.py
+++ Module1/practice/points/01_line.py
@@ -2,7 +2,20 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
+
+# Ломаная линия задана последовательным списком точек
+points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
+
+# Задание: Найдите длину ломаной линии
+
+# TODO: your core here...
+
+print("Длина ломаной линии = ", ...)
 
 # Ломаная линия задана последовательным списком точек
 points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
